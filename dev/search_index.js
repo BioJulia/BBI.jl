2ea8ed4fb6815ae574259ead92b7c4be27a62d15
autodocs <autodocs>
2019-01-17 03:32:34 +0000

    build based on 61b586c

---
+++ dev/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#BBI.jl-1",
+    "page": "Home",
+    "title": "BBI.jl",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "Install BBI from the Julia REPL:julia> Pkg.add(\"BBI\")If you are interested in the cutting edge of the development, please check out the master branch to try new features before release."
+},
+
+]}
